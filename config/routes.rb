689ffcf7a9eda20fb4df2e69751fee82b2305f82
cuongpm <harry88pham@gmail.com>
2016-04-23 21:29:17 +0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-  root 'home#login'
-  get 'wellcome', to: 'home#wellcome'
-  get 'step2', to: 'home#step2'
+  root 'home#index'
   get '/conversation', to: 'home#index'
-=======
-  root 'home#index'  
+
 
   get '/welcome', to: 'welcome#index'
   get '/welcome/step1', to: 'welcome#step1'
   get '/welcome/step2', to: 'welcome#step2'
->>>>>>> 0ef63667c06562c2bf4bce1a555626701e5abb5e
+
 
   mount ShopifyApp::Engine, at: '/'
 
